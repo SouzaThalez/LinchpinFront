a368eb70d075f056d10eb2f8351c971dc0e0ec6f
Thales <selath10@gmail.com>
2024-08-31 17:21:04 -0300

    home and profile page layout edited

--- src/app/components/private/home/home.component.ts
+++ src/app/components/private/home/home.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  months = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+  ];
+
+  ngOnInit(): void {
+    this.callGeneralChart();
+  }
+
+
+    //Graphics
+    private callGeneralChart(){
+    
+      const ctx = document.getElementById('generalChart') as HTMLCanvasElement;
+      new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: this.months,
+          datasets: [{
+            label: 'N° relatórios',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          },
+          plugins: {
+             legend: {
+                display: false
+              }
+          }
+        }
+      });
+  
+      
+  
+     }
+
 
 }
--- src/app/components/private/profile/profile.component.ts
+++ src/app/components/private/profile/profile.component.ts
@@ -1,10 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart,registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
+
+  months = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+  ];
+
+
+  ngOnInit(): void {
+    this.callChart();
+    this.cleaningChart();
+  }
+
+
+
+   //Graphics
+   private callChart(){
+    
+    const ctx = document.getElementById('manitanceChart') as HTMLCanvasElement;
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: this.months,
+        datasets: [{
+          label: '',
+          data: [12, 19, 3, 5, 2, 3],
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        },
+        plugins: {
+           legend: {
+              display: false
+            }
+        }
+      }
+    });
+
+    
+
+   }
+
+   private cleaningChart(){
+    
+    const ctx = document.getElementById('cleaningChart') as HTMLCanvasElement;
+    new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: this.months,
+        datasets: [{
+          label: '',
+          data: [12, 19, 3, 5, 2, 3],
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        },
+        plugins: {
+           legend: {
+              display: false
+            }
+        }
+      }
+    });
+
+    
+
+   }
+
+
 
 }
