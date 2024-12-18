c09a4102005a96997c9163ccceca09f6fe012d9b
Thales <selath10@gmail.com>
2024-12-18 18:46:47 -0300

    firebase installed

--- src/app/components/private/private.module.ts
+++ src/app/components/private/private.module.ts
@@ -12,7 +12,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { HttpClientModule } from '@angular/common/http';
-import { ProfileComponent } from './profile/profile.component';
+
 
 
 @NgModule({
@@ -32,7 +32,8 @@ import { ProfileComponent } from './profile/profile.component';
     MatButtonModule,
     MatSelectModule,
     MatInputModule,
-    HttpClientModule
+    HttpClientModule,
+  
   ]
 })
 export class PrivateModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://your-firebase-endpoint.com' // Firebase URL
+    firebaseConfig: {
+      apiKey: "AIzaSyB0UC0dKbKCTKP5F1fJSlEWj5mlVrI76xI",
+      authDomain: "linchpin-38e6f.firebaseapp.com",
+      projectId: "linchpin-38e6f",
+      storageBucket: "linchpin-38e6f.firebasestorage.app",
+      messagingSenderId: "212205551703",
+      appId: "1:212205551703:web:1c83dc4748f7c718296312",
+      measurementId: "G-SMKZGG8RPN"
+    }
+    
   };
